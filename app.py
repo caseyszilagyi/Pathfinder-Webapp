5499f55c009919d36c9df45e1892f2eb96501d44
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-05-13 19:40:48 -0400

    got wall clicking and backend calls fully working

--- app.py
+++ app.py
@@ -27,18 +27,28 @@ def updateBlock():
 @socketio.on('start')
 def start_program():
     print('Started program', file=sys.stderr)
-    change_cell(5, 5, "visited")
+    change_cell_coords(5, 5, "visited")
 
-# Call this method to change the cell to a different type
+
+# Called from the front end to change a cell
+@socketio.on('change_cell')
+def change_cell_frontend(data):
+    change_cell(data['ID'], data['type']);
+
+# Call this method to change the cell to a different type. Calls method below by converting coordinates to proper format
 # Currently implemented types are unvisited, visited, wall, path
-def change_cell(x, y, changeType):
+def change_cell_coords(x, y, changeType):
     blockID = str(y) + ":" + str(x)
+    change_cell(blockID, changeType)
+
+## Called by the method above, actually will change the cell display on the front end
+def change_cell(blockID, changeType):
     print("Change cell start")
     emit('change_cell', {'ID': blockID, 'type': changeType})
     print("Change cell end")
 
 
-# Test method
+# Test method that prints whatever is need
 @socketio.on('test')
 def test(data):
     print("Test Callback, data: " + data["ID"], file=sys.stderr)
