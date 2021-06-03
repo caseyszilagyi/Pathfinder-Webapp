fd0879dfa30960eed6dd157d3ab20ba63fff9a02
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-06-02 21:55:37 -0400

    debug line

--- static/index.js
+++ static/index.js
@@ -3,7 +3,7 @@
     var http = require('http').createServer(app);
     var io = require('socket.io')(http);
     var PORT = process.env.PORT || 3000;
-
+    console.log("yo");
 
     http.listen(PORT,function(){
         console.log("Listening to port " + PORT);
